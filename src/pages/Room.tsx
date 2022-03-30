8f754c8b41e916ea1cc56700fa7708cf93324b68
Hiroshi Gonçalves Hashimoto <hiroshi.hisako@gmail.com>
2022-03-29 22:26:59 -0300

    Create Questions Page

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -47,7 +47,7 @@ export function Room() {
 
   useEffect(() => {
     const roomRef = database.ref(`rooms/${roomId}`);
-    roomRef.once('value', room => {
+    roomRef.on('value', room => {
       const databaseRoom = room.val();
       const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
       const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
@@ -103,8 +103,8 @@ export function Room() {
 
       <main>
         <div className="room-title">
-          <h1>Sala React</h1>
-          <span>4 perguntas</span>
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
         </div>
 
         <form onSubmit={handleSendQuestion}>
@@ -125,6 +125,8 @@ export function Room() {
             <Button type="submit" disabled={!user}>Enviar pergunta</Button>
           </div>
         </form>
+
+        {JSON.stringify(questions)}
       </main>
     </div>
   );
